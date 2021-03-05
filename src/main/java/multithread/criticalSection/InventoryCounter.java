eff52f06e9e51509a31b8857ff6d2c62380632c8
yifeiliu <yfliu87@gmail.com>
2021-03-04 22:26:14 -0800

    add critical section example

---
+++ src/main/java/multithread/criticalSection/InventoryCounter.java
@@ -0,0 +1,25 @@
+package multithread.criticalSection;
+
+public class InventoryCounter {
+    private int items = 0;
+
+    Object lock = new Object();
+
+    public void increment() {
+        synchronized (this.lock) {
+            items++;
+        }
+    }
+
+    public void decrement() {
+        synchronized (this.lock) {
+            items--;
+        }
+    }
+
+    public int getItems() {
+        synchronized (this.lock) {
+            return this.items;
+        }
+    }
+}
